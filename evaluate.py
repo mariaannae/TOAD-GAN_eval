258dfecdefed914834614b06c9c8de7714a2518a
mariaannae <maria.edwards@gmail.com>
2021-04-20 13:40:31 -0400

    fixing evaluate script

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,16 @@
+#import random
+from mario.level_utils import one_hot_to_ascii_level
 
-def platform_test_vec(vec):
-    platform1 = vec[:,:,-1,:]
-    platform2 = vec[:,:,-2,:]
-    mismatch = (abs(platform1-platform2)>.05).sum()
-    #need to render them
-    if mismatch > 0:
-        return 1
-    else: return 0
+def platform_test_vec(vec, token_list):
+    score = 0.0
+
+    ascii_level = one_hot_to_ascii_level(vec.detach(), token_list)
+    for i in range(len(ascii_level[-1])):
+        if ascii_level[-1][i] != ascii_level[-2][i]:
+            score+=1.0
+    
+    return score
+
 
 def platform_test_file(path_to_level_txt):
     n_lines = 0
